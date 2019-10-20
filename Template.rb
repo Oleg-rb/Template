3a83e69a8e682592245f1b2f24257f13d7a06088
Oleg-rb <oleg1107@rambler.ru>
2019-10-20 14:17:45 +0300

    Changed veb adress

--- Template.rb
+++ Template.rb
@@ -5,5 +5,5 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/Oleg-rb/Template/\">Original</a> pattern has been modified for <a href=\"https://github.com/Oleg-rb/Template/\">Template</a>"			
 end
